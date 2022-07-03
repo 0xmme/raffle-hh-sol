38f3dda0d86989b5ef84d388b06cd43b580c0e72
mme <9083787+mme022@users.noreply.github.com>
2022-07-03 19:13:26 +0200

    commit before implementing chainlink VRF request

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -12,13 +12,23 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.8;
 
+//import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+
+/* Errors */
 error Raffle__NotEnoughEthEntered();
 
-contract Raffle {
+contract Raffle is VRFConsumerBaseV2 {
+    /* state variables */
     uint256 private immutable i_EntranceFee;
     address payable[] private s_Players;
 
-    constructor(uint256 entranceFee) {
+    /* Events */
+    event RaffleEnter(address indexed player);
+
+    constructor(address vrfCoordinatorV2, uint256 entranceFee)
+        VRFConsumerBaseV2(vrfCoordinatorV2)
+    {
         i_EntranceFee = entranceFee;
     }
 
@@ -27,9 +37,17 @@ contract Raffle {
             revert Raffle__NotEnoughEthEntered();
         }
         s_Players.push(payable(msg.sender));
+        emit RaffleEnter(msg.sender);
     }
 
-    //function pickWinner() public {}
+    function requestRandomWinner() external {}
+
+    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords)
+        internal
+        override
+    {}
+
+    /* view / pure functions */
     function getEntranceFee() public view returns (uint256) {
         return i_EntranceFee;
     }
